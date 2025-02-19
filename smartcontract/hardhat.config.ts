542d173cccd4c5ad4922cc07158e35aad1c7e3cd
U~Iriamuzu <daviswill264@gmail.com>
2025-02-19 19:15:47 +0100

    DONE

--- smartcontract/hardhat.config.ts
+++ smartcontract/hardhat.config.ts
@@ -5,7 +5,7 @@ import "hardhat-deploy";
 import "dotenv/config";
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
-const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY || "";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
@@ -14,11 +14,11 @@ const config: HardhatUserConfig = {
       chainId: 31337,
     },
     sepolia: {
-      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      url: `https://sepolia.alchemy.io/v3/${ALCHEMY_API_KEY}`,
       accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
     },
     baseSepolia: {
-      url: `https://base-sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      url: `https://base-sepolia.alchemy.io/v3/${ALCHEMY_API_KEY}`,
       accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
     },
   },
